620de9a2b8da2ec8b2f29c0ca0f07c47b904ae0e
richardjohns <richard.johns193@gmail.com>
2017-08-30 19:11:18 +1200

    Create eloquent.js

---
+++ eloquent.js
@@ -0,0 +1,8 @@
+// Palindrome checker
+
+function palindrome(str) {
+  return str.replace(/[\W_]/g, '').toLowerCase() ===
+         str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
+}
+
+palindrome("eye");
