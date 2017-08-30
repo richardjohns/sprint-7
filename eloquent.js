73e84b308d5ca43a7f5655bbbb66d6c21c4c8d5f
richardjohns <you@example.com>
2017-08-30 19:16:28 +1200

    add eloquent.js

--- eloquent.js
+++ eloquent.js
@@ -1,3 +1,37 @@
+// Reverse a string
+
+function reverseString(str) {
+  var strArray = str.split("");
+  var strRev = strArray.reverse();
+  str = strRev.join("");
+  return str;
+}
+
+reverseString("hello");
+
+
+// Factorialise a number
+
+function factorialize(num) {
+  var counter = num;
+  var resultSum = 0;
+
+  if (num === 0) {
+  return resultSum = 1;
+  } else {
+  for(i = 1; i < counter; i++) {
+  console.log(i);
+  console.log(counter);
+  resultSum = num *= i;
+  console.log(resultSum);
+  }
+
+  return resultSum;
+  }
+}
+
+console.log(factorialize(5));
+
 // Palindrome checker
 
 function palindrome(str) {
