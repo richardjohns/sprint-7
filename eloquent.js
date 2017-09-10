2d3f4024f7dc91062999ace4624f26241082e28c
richardjohns <you@example.com>
2017-09-10 21:43:06 +1200

    update eloquent.js

--- eloquent.js
+++ eloquent.js
@@ -40,3 +40,39 @@ function palindrome(str) {
 }
 
 palindrome("eye");
+
+//FIND THE LONGEST WORD IN A STRING
+
+function findLongestWord(str) {
+  var strArray = str.split(' ');
+    console.log(strArray);
+  var lengthArr = [];
+  for (i = 0; i < strArray.length; i++) {
+  lengthArr.push(strArray[i].length);
+    console.log(lengthArr);
+  }
+    function getMaxOfArray(lengthArray) {
+      return Math.max.apply(null, lengthArray);
+    }
+
+    var maxVal = getMaxOfArray(lengthArr);
+    return maxVal;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+//TITLE CASE A SENTENCE
+
+function titleCase(str) {
+    var strArray = str.split(' ');
+    console.log(strArray);
+    var uppercaseArray = [];
+        for (i = 0; i < strArray.length; i++) {
+        uppercaseArray.push(strArray[i].charAt(0).toUpperCase() + strArray[i].slice(1).toLowerCase());
+        console.log(uppercaseArray);
+        }
+    var joinString = uppercaseArray.join(" ");
+  return joinString;
+}
+
+titleCase("I'm a little tea pot");
